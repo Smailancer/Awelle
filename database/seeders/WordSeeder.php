c361350d147d18918fe6e0a8bdd3cfed0b389310
smail <b.smailiams@gmail.com>
2023-12-04 22:48:24 +0100

    add import csv option

--- database/seeders/WordSeeder.php
+++ database/seeders/WordSeeder.php
@@ -18,102 +18,30 @@ class WordSeeder extends Seeder
         // Assuming you have a user with the username 'Admin' as per your DatabaseSeeder
         $user = User::where('username', 'Admin')->first();
 
-        $wordsData = [
-            'Kabyle' => [
-                [
-                    'term' => 'ثقشيشين',
-                    'slug' => 'Thiq-sheesh-een',
-                    'ar_meaning' => 'البنات',
-                ],
-                [
-                    'term' => 'أراش',
-                    'slug' => 'Arrāsh',
-                    'ar_meaning' => 'الذكور',
-                ],
-                [
-                    'term' => 'ايغورذان',
-                    'slug' => 'Aigurḍān',
-                    'ar_meaning' => 'الصغار',
-                ],
-                [
-                    'term' => 'ثمطوث',
-                    'slug' => 'Thamaṭṭūth',
-                    'ar_meaning' => 'امرأة',
-                ],
-                [
-                    'term' => 'ارغاز',
-                    'slug' => 'Argāz',
-                    'ar_meaning' => 'رجل',
-                ],
-                [
-                    'term' => 'أمداكليو',
-                    'slug' => 'Amaddākliw',
-                    'ar_meaning' => 'صديقي',
-                ],
-                [
-                    'term' => 'ثمدكلتيو',
-                    'slug' => 'Thamaddākultiw',
-                    'ar_meaning' => 'صديقتي',
-                ],
-            ],
-            'Mozabit' => [
-                [
-                    'term' => 'تيزيوين',
-                    'slug' => 'Tiziouine',
-                    'ar_meaning' => 'البنات',
-                ],
-                [
-                    'term' => 'إضفلاين',
-                    'slug' => 'Ideflaine',
-                    'ar_meaning' => 'الذكور',
-                ],
-                [
-                    'term' => 'إبزانن',
-                    'slug' => 'Ibezanen',
-                    'ar_meaning' => 'الصغار',
-                ],
-                [
-                    'term' => 'تمطوت',
-                    'slug' => 'Tamaṭṭūt',
-                    'ar_meaning' => 'امرأة',
-                ],
-                [
-                    'term' => 'ارجاز',
-                    'slug' => 'Arjāz',
-                    'ar_meaning' => 'رجل',
-                ],
-                [
-                    'term' => 'أمدوتشليك',
-                    'slug' => 'Amaddotchlik',
-                    'ar_meaning' => 'صديقي',
-                ],
-                [
-                    'term' => 'تمدوتشلتيك',
-                    'slug' => 'Tamaddotchlik',
-                    'ar_meaning' => 'صديقتي',
-                ],
-            ],
-            // Add other slangs as needed
-        ];
 
-        foreach ($wordsData as $slangName => $slangWords) {
+        $csvFile = database_path('seeders/words.csv');
+        $wordsData = array_map('str_getcsv', file($csvFile));
+
+        foreach ($wordsData as $row) {
+            list($term, $slug, $ar_meaning, $slangName) = $row;
+
+            // Find or create slang
             $slang = Slang::where('name', $slangName)->first();
-
-            if ($slang) {
-                foreach ($slangWords as $wordData) {
-                    Word::create([
-                        'user_id' => $user->id,
-                        'term' => $wordData['term'],
-                        'slug' => $wordData['slug'],
-                        'ar_meaning' => $wordData['ar_meaning'],
-                    ])->slang()->attach($slang->id);
-
-
-                }
-                $this->command->info("Words seeding completed for the: {$slang->name} slang");
-            } else {
+            if (!$slang) {
                 $this->command->error("Slang not found for name: $slangName");
+                continue;
             }
+
+            // Create word
+            Word::create([
+                'user_id' => $user->id,
+                'term' => $term,
+                'slug' => $slug,
+                'ar_meaning' => $ar_meaning,
+            ])->slang()->attach($slang->id);
+
+            $this->command->info("Seeding completed for term: $term");
+
         }
     }
 
--- resources/views/components/words-grid.blade.php
+++ resources/views/components/words-grid.blade.php
@@ -1,11 +1,22 @@
 @props(['words'])
 
+<div class="lg:grid lg:grid-cols-3">
 
-    <div class="lg:grid lg:grid-cols-3">
+    {{-- Pin specific words by their slugs --}}
+    @foreach (['awelle', 'Adhlamdagh', 'taghenjayt'] as $pinnedSlug)
         @foreach ($words as $word)
-            <x-word-card
-                :word="$word"
-            />
+            @if ($word->slug === $pinnedSlug)
+                <x-word-card :word="$word"/>
+            @endif
         @endforeach
-    </div>
+    @endforeach
+
+    {{-- Display the rest of the words --}}
+    @foreach ($words as $word)
+        @if (!in_array($word->slug, ['slug1', 'slug2', 'slug3']))
+            <x-word-card :word="$word"/>
+        @endif
+    @endforeach
+
+</div>
 
